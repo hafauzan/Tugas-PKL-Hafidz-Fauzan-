745baa3ffa4d6d27c11302b2e4b36ed6f501f2b9
hafauzan <80023557+hafauzan@users.noreply.github.com>
2021-03-04 15:35:27 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>Informasi Pribadi</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="storage/fotoku.jpg" align="right" width="104" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Muhammad Hafidz Fauzan </td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir</td>
+            <td>: Sukabumi, 01 Mei 2004 </td>  
+        </tr>
+            <td>Alamat</td>
+            <td>: Jl. Tata Nugraha No.78 , Cicadas Girang 03/03, Jayamekar, Baros</td>
+        <tr>
+            <td>Status</td>
+            <td>: Pelajar</td>
+        </tr>
+            <tr>
+            <td>NO HP</td>
+            <td>: 083805214272</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: muhafauzan14@gmail</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>PENDIDIKAN FORMAL</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2023</td>
+                <td>SMK Negeri 2 Sukabumi </td>
+            </tr>
+            <tr>
+                <td>2016-2019</td>
+                <td>SMP Negeri 1 Sukabumi </td>  
+            </tr>
+                <td>2010-2016</td>
+                <td>SDN Cicadas Girang</td>
+            <tr>
+                <td>2008-2010</td>
+                <td>TK Al-Inayah</td>
+            </tr>
+            </table>
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANISASI</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2020</td>
+                            <td>Ketua Ekstrakulikuler Bahsa</td>
+                        </tr>
+                        <tr>
+                            <td>2020</td>
+                            <td>Judat Ambalan Kiansantang </td>  
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>KEAHLIAN</b><hr>
+                        Mengoperasikan Komputer : Microsoft Office<br>
+                        Desain Grafis: CorelDRAW <br>
+    <a href="indeks.html">KEMBALI KE INDEKS</a><br>
+    </body>
+</html>
